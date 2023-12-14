1f2543db4f3545fcd93e407163d7c139066be7e2
Dominik Kołodziej <dominik_kolodziej@protonmail.com>
2023-12-14 19:42:06 +0100

    init

---
+++ src/main/java/com/ecobank/api/EcoBankSaApplication.java
@@ -0,0 +1,13 @@
+package com.ecobank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoBankSaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoBankSaApplication.class, args);
+    }
+
+}
